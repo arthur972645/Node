3c897f51e74346cb2605af87e79b6c045f07d147
arthur972645 <arthurall.1973@gmail.com>
2024-04-10 19:04:23 -0300

    api comentada

--- node/APIs/1_HTTP_ARRAY/server.js
+++ node/APIs/1_HTTP_ARRAY/server.js
@@ -11,32 +11,52 @@ exclui um dadao(ROUTE PARMS) - (PUT, PATH, DELETE)
 http://localhost:3333/user/1
 */
 
-import { NONAME } from 'node:dns'
+//Modulo interno do Node.js sendo importado, esse modulo permite a criação de um servidor
 import http from 'node:http'
 
+//Definindo para a variavel PORT, qual sera a porta em que o servidor vai ser executado 
 const PORT = 3333
 
+//array que será usado para guardar os dados dos usuarios
 const users = []
 
+//criando um servidor atraves do metodo createServer, onde vai ter uma função que sempre será chamda quando for feito uma requisição ao servidor
+//Essa função vai retornar o parametro request - reprenta a requisição http recebida
+//Essa função vairetornar o parametro response - representear a resposta enviado de volta
 const server = http.createServer((request, response) => {
+    //to extraindo o metodo da solitação e o caminho da solicitação
     const {method, url} = request
 
+    //estamos querendo listar todos os usuarios cadastrados, com isso fazemos um validação para saber se é isso mesmo. Essa validação é feita da seguinte forma, se a url = caminho  for igual /users e se o metodo ustilizado para puxar esses dados for o GET, significa que o usuario ta pedidno de volta todos os usuarios cadastrados
+    //vai veririficar se ta puxando do users
     if(url === '/users' && method === "GET"){//Buscar todos os usuários
+        //É dessa forma que vc retrona os dados pro usuarios 
         response.setHeader('Content-Type', 'application/json')
         response.end(JSON.stringify(users))
     }
+    
+   
     else if(false){//buscar um uniico usuasrio
+     //Aqui vaos adicionar um novo usuario, para isso é feito uma validação para saber se vai ser adicionado no caminho /users e se o metodo utulizado para adcionar - é o POST, caso for, vai se feita a logica para adionar um novo usuario
+     //vai verificar se ta adcionado no users
     }else if (url === '/users' && method === "POST"){ //cadastrando um usuario
+        //vai servir para armazenar os dados que estão sendo enviado pro corpo da solicitação, ou seja, os usuarios
         let boby = ''
+        //vai ser envaido dados, essa função vai ler os dados e converter do tipo shunk para sting e anexar no body
         request.on('data',(chunk) => {
             boby += chunk.toString()
         })
+        //idica que que o body ja ta completo, ou seja, com todos os dados recebidos e podemos continuar a operação
         request.on('end',() => {
+            //estamos convertendo os dadso recebidos do body para objeto js, ataves do JSON.parse()
             const novoUsuario = JSON.parse(boby)
             novoUsuario.id = '1'
+            //adicoando o novoUsuario para o array users
             users.push(novoUsuario)
 
+            //idicar que o recurso foi criado com sucesso e é um objeto JSON
             response.writeHead(201, {'Content-Type' : 'application/json'})
+            //enviando a resposta a resposta de volta ao cliente, onde a gente converte o novo usuario que está como objeto Json e enviamos pro corpo da resposta
             response.end(JSON.stringify(novoUsuario))
         })
     } 
